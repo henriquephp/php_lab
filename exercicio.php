9c07db4143a2c3d6582011800d352b1ba00e7bb1
henriquephp <henriqueb.gregorio@outlook.com.br>
2022-08-30 21:39:00 -0300

    comit exercicios

--- exercicio.php
+++ exercicio.php
@@ -1,2 +1,29 @@
 <?php
 
+$contDiv = 0;  // 
+$primos = []; 
+
+$limite = 12;
+
+for ($numero = 3; $numero < $limite; $numero++) {
+    $contDiv = 0;
+
+    // echo "numero: " . $numero . "<br>";
+
+    for ($numeroDescontar = $numero; $numeroDescontar > 1; $numeroDescontar--) {
+        // echo "numeroDescontar: " . $numeroDescontar . "<br>";
+
+        $resto = $numero % $numeroDescontar;
+
+        if ($resto == 0) {
+            $contDiv++;
+        }
+    }
+
+
+    if ($contDiv == 1) {
+        $primos[] = $numero;
+    }
+}
+
+echo implode(",", $primos);
